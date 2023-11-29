44dc3ec2a2e417aea665e8970ab9cc0875ada385
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-11-29 15:42:30 -0800

    added sessons

--- playdates-frontend/src/pages/Admin.js
+++ playdates-frontend/src/pages/Admin.js
@@ -1,39 +1,48 @@
 import React, { useState, useEffect } from 'react';
 
 export const Admin = () => {
+  const [authenticated, setAuthenticated] = useState(false);
   const [htmlContent, setHtmlContent] = useState('');
 
-  useEffect(() =>
-  fetch('http://localhost:3000/admin'))
-
   useEffect(() => {
-    // Fetch HTML content from the backend when the component mounts
-    fetch('http://localhost:3000/admin')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network could not respond');
+    // Check if the user is authenticated
+    fetch('http://localhost:3000/admin', {
+      method: 'GET',
+      credentials: 'include', // Include credentials (cookies) for cross-origin requests
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.authenticated) {
+          // If authenticated, set the state and proceed to fetch HTML content
+          setAuthenticated(true);
+
+          // Fetch HTML content of the admin page
+          fetch('http://localhost:3000/admin/content', {
+            method: 'GET',
+            credentials: 'include', // Include credentials (cookies) for cross-origin requests
+          })
+            .then((response) => response.text())
+            .then((html) => {
+              setHtmlContent(html);
+            })
+            .catch((error) => console.error('Error fetching HTML content', error));
+        } else {
+          setAuthenticated(false);
         }
-        return response.text();
       })
-      .then((html) => {
-        console.log('Received HTML content', html);
-        setHtmlContent(html);
-      })
-      .catch((error) => {
-        console.error('Error fetching HTML content', error);
-      });
-  }, []); // Empty dependency array so this effect only runs once
+      .catch((error) => console.error('Error checking authentication', error));
+  }, []);
 
   return (
     <div>
-      {htmlContent ? (
+      {authenticated ? (
         // Render the HTML content from the backend using dangerouslySetInnerHTML
         <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
       ) : (
         <p>No clients have registered yet!</p>
       )}
     </div>
-  ); 
-}
+  );
+};
 
 export default Admin;
