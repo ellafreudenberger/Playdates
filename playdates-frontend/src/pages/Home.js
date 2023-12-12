9f355f75c88e00a0aceac169f9fb3cbba468c111
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-12-11 19:16:35 -0800

    added an image to homepage

--- playdates-frontend/src/pages/Home.js
+++ playdates-frontend/src/pages/Home.js
@@ -3,12 +3,18 @@ import "../index.css";
 import { Link } from 'react-router-dom';
 
 
+
 export const Home = () => {
     return (
         <div> 
     <h1 className="companyTitle"> Playdates</h1>
     <h1 className="homeMainTitle">Your neighborhood dog sitters!</h1> 
     <h2 className="homeDescription"> Set your dog up on fun&nbsp;<span className="playdatesText">Playdates</span>&nbsp;with us! We offer services for local walks, sittings at your home and boardings at ours in the heart of the Upper East Side. We are dedicated to providing exceptional care and ensuring your pet's well-being and happiness. <Link to="/register">Register</Link> to book our services now, or if you would like to meet us before, contact us at our information below to schedule a consultation! </h2>
+   <img src="/images/Dog23.jpeg"
+        alt="Ella"
+        className="homeDog1"
+        height="700"
+        />
     <div className="homeServicesBorder">
     <h3 className="homeServices">Our Services</h3>
         <h4 className="homeTitle">Dog Walking</h4>
